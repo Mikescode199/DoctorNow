40641086d43a212ce39963d007cc1057fbdf32db
mikecardona076 <mikecardona076@gmail.com>
2020-11-06 08:34:58 -0800

    API BRAEKING BAD

--- componente/class_views.py
+++ componente/class_views.py
@@ -1,18 +1,26 @@
 from django.views.generic import ListView, TemplateView
 import requests
+import json
 
+def generate_request(url, params={}):
+    response = requests.get(url, params=params)
 
+    if response.status_code == 200:
+        return response.json()
+    
+    
 class Menu(TemplateView):
     template_name = 'examples/dashboard.html'
     
     def get(self, request, *args, **kwargs):
         self.object = None
-        url = requests.get('https://api.imgflip.com/get_memes')
-        response = requests.get(url)
-        obj = response.json()
-
+        params = { 'order': 'desc' }
+        response = generate_request('https://breaking-bad-quotes.herokuapp.com/v1/quotes', params)
+        data_string = json.dumps(response)
+        decoded = json.loads(data_string)
         return self.render_to_response(
-               self.get_context_data(
-               name = obj["id"]
+            self.get_context_data(
+            noticia = decoded[0]['quote'],
+            autor = decoded[0]['author']
             ))
 
